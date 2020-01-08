b498cc5b2f2de8b3e5b6c6a4541abd845f78ebdd
fanyoufu <fanyoufu@outlook.com>
2020-01-08 15:57:29 +0800

    实现登陆功能

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,7 +5,7 @@
     />
     <van-cell-group>
       <van-field
-
+        v-model="user.mobile"
         required
         clearable
         label="手机号"
@@ -14,7 +14,7 @@
       />
 
       <van-field
-
+        v-model="user.code"
         type="password"
         label="验证码"
         placeholder="请输入验证码"
@@ -22,18 +22,56 @@
       />
     </van-cell-group>
     <div class="wrap">
-      <van-button type="primary" class="btn">登陆</van-button>
+      <van-button type="primary" class="btn" @click="hLogin">登陆</van-button>
 
     </div>
   </div>
 </template>
 
 <script>
+import request from '@/utils/request'
 export default {
   name: '',
   data () {
     return {
+      user: {
+        mobile: '13412341234',
+        code: '246810'
+      }
+    }
+  },
+  methods: {
+    async hLogin () {
+      // 对用户的信息进行校验
+      // todo: 更加完备验证
+      if (this.user.mobile === '') {
+        this.$toast.fail('手机号不能为空')
+        // return
+      }
 
+      try {
+        this.$toast.loading({
+          duration: 0,
+          message: '登陆中...',
+          forbidClick: true,
+          loadingType: 'spinner'
+        })
+
+        // 发请求
+        const result = await request({
+          url: '/app/v1_0/authorizations',
+          method: 'post',
+          data: this.user
+        })
+
+        // 根据返回结果做后续的处理
+        console.log(result.data)
+        this.$toast.success('登陆成功')
+      } catch (err) {
+        if (err && err.response && err.response.status === 400) {
+          this.$toast.fail('登陆失败')
+        }
+      }
     }
   }
 }

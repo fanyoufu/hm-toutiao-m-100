39bf0f4b902fd7bef2e854bc4374129bde434df9
fanyoufu <fanyoufu@outlook.com>
2020-01-08 11:18:19 +0800

    引入vant组件

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
   <div id="app">
     <div id="nav">
       主页
+      <van-button type="warning" ></van-button>
+
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="info">信息按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+      <van-button type="danger">危险按钮</van-button>
     </div>
     <router-view/>
   </div>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入vant样式
+import 'vant/lib/index.css'
+
+// 手动按需引入组件
+import { Button } from 'vant'
+// 注册组件
+Vue.use(Button)
+
 Vue.config.productionTip = false
 
 new Vue({

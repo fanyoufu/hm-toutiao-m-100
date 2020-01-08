4442c2853a5a9daf0238c0017a2d796002b70cb9
fanyoufu <fanyoufu@outlook.com>
2020-01-08 15:17:12 +0800

    结构及路由设置

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      主页
-
-      <van-button type="danger">危险按钮</van-button>
-    </div>
     <router-view/>
   </div>
 </template>
 <script>
-import request from '@/utils/request'
-export default {
-  created () {
-    console.log(request)
-    request({
-      method: 'get',
-      url: '/app/v1_0/channels'
-    })
-  }
-}
+// import request from '@/utils/request'
+// export default {
+//   created () {
+//     console.log(request)
+//     request({
+//       method: 'get',
+//       url: '/app/v1_0/channels'
+//     })
+//   }
+// }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import Login from '@/views/login'
 Vue.use(VueRouter)
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'home',
-  //   component: Home
-  // },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  }
   // {
   //   path: '/about',
   //   name: 'about',
